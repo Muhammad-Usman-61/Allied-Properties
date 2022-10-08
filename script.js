9b2b059e223f8b77bb019978ef70948adb8bcf79
Muhammad-Usman-61 <muhammad196usman.com>
2022-10-08 10:28:27 +0500

    hero part done

---
+++ script.js
@@ -0,0 +1,21 @@
+// Hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header_content");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
